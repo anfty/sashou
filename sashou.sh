6e5d50d7d7968a53bcc0781130a5f2596468f861
Poli <84727694+anfty@users.noreply.github.com>
2022-05-20 00:34:16 +0800

    优化流程和代码
    简化了部分代码，优化了流程

--- sashou.sh
+++ sashou.sh
@@ -10,11 +10,11 @@
 
 ## 通常修改网卡与宽带参数即可联网
 ## account 宽带账号，passwd 宽带账号对应密码
-account="1234567890"
-passwd="1234567890"
+account="account"
+passwd="passwd"
 
 ## 网卡
-network_card="eth0.2"
+network_card="wan"
 
 ## 自动时间戳
 dr="dr"$(date +%s)
@@ -27,11 +27,10 @@ user_ip=""
 
 ## led status
 # tmp
-tmp_led_status=""
-# true
-led_status=""
+
 
 function get_user_ip(){
+	# 获取网卡对应ip
 	tmp=$(ip addr|grep $network_card$|cut -d/ -f1|cut -d" " -f6)
 	if [ "$tmp" == "" ] ;then
 		return 0
@@ -49,29 +48,29 @@ do
 			if [ "$OPTARG" != "" ] ;then
 				account=$OPTARG
 			fi
-		;;
+			;;
         p)
             if [ "$OPTARG" != "" ] ;then
                 passwd=$OPTARG
             fi
-        ;;
+        	;;
         u)
             if [ "$OPTARG" != "" ] ;then
                 user_ip=$OPTARG
             fi
-        ;;
+        	;;
         s)
             if [ "$OPTARG" != "" ] ;then
 			s=$OPTARG
             fi
-		;;
+			;;
         n)
             if [ "$OPTARG" != "" ] ;then
 				if [ "$user_ip" == "" ] ;then
 					user_ip=$(ip addr|grep $OPTARG$|cut -d/ -f1|cut -d" " -f6)
 				fi
         	fi
-        ;;
+        	;;
 		?)
 			echo "	SASHOU automatic authentication script"
 			echo "		-a : 宽带账号"
@@ -85,6 +84,9 @@ do
 	esac
 done
 
+
+
+
 # 其他设备已在线
 # {"result":"0","msg":"aW51c2UsIGxvZ2luIGFnYWluLCBwYyBPTG5vIDEgPj0gMQ==","ret_code":4}
 # 登录成功
@@ -92,115 +94,119 @@ done
 # 已在线
 # {"result":"0","msg":"","ret_code":2}
 
-#路由器状态（led状态）:未获得ip（快速闪烁），断开网络（间隔双闪），正在链接（呼吸闪烁），网络在线（隔一段时间亮灯一次）,对应网络状态0,1，2，3
+
 function route_status_led(){
-	if [ "$(uname -a|grep OpenWrt)" != "" ] ;then
-		case $1 in
-			connecting )
-				(mt1300_led blue_flash fast)
-				;;
-			ap_drops )
-				(mt1300_led blue_flash normal)
-				;;
-			ip_drops )
-				(mt1300_led white_flash normal)
-				;;
-			error )
-				(mt1300_led blue_breath)
-				;;
-			off )
-				(mt1300_led off)
-				;;
-		esac
+	if [ "$led_status" == "$1" ] ;then
+		return 0
+	else
+		led_lock=1
+		if [ "$1" == "off" ] ;then
+			led_lock=0
+		fi
+		led_status=$1
+		# route led status
+		if [ "$(uname -a|grep 'GL-MT1300')" != "" ] ;then
+			case $1 in
+				connecting )
+					(mt1300_led blue_flash fast)
+					;;
+				ap_drops )
+					(mt1300_led blue_flash normal)
+					;;
+				ip_drops )
+					(mt1300_led white_flash normal)
+					;;
+				error )
+					(mt1300_led blue_breath)
+					;;
+				off )
+					(/etc/init.d/led stop)
+					;;
+			esac
+		fi
 	fi
 }
 
+
 # route_log
 function route_log(){
+	# 存储日志 建议修改路径
 	echo "$(date)  info: $1" >> /root/sashou/.route.log
 }
 
-#网络状态检测,网络连通返回1，反之返回0
+
 function network_status(){
-	#$(ping baidu.com -w 1 -c 1 2>/dev/null | wc -l)
-	if [ $(ping baidu.com -w 1 -c 1 2>/dev/null | wc -l) == 6 ] ;then
+	#网络状态检测,网络连通返回1，反之返回0 182.254.225.219
+	if [ $(timeout 1 ping baidu.com -w 1 -c 1 2>/dev/null | wc -l) == 6 ] ;then
+		return 1
+	elif [ $(timeout 1 ping 182.254.225.219 -w 1 -c 1 2>/dev/null | wc -l) == 6 ] ;then
 		return 1
 	fi
 	return 0
 }
 
+
+
 function getnetwork(){
-	network_status
-	if [ $? == 1 ] ;then
-		if [ "$led_status" != "off" ] ;then
-			led_status="off"
-			route_status_led $led_status
-		fi
-		return 0
-	fi
-	# network error
-	if [ "$led_status" != "connection" ] ;then
-		tmp_led_status="connecting"
-		route_status_led $tmp_led_status
-	fi
-
 	url='http://110.188.66.35:801/eportal/?c=Portal&a=login&callback='$dr'&login_method=1&user_account=%2C0%2C'$account'&user_password='$passwd'&wlan_user_ip='$user_ip'&wlan_user_ipv6=&wlan_user_mac=000000000000&wlan_ac_ip=&wlan_ac_name=&jsVersion=3.1&_='
-
+	# 登录请求
 	rep=$(curl -s $url --connect-timeout $s|grep -o '{.*}')
 	if [ "$rep" == '{"result":"0","msg":"","ret_code":2}' ] ;then
 		# 已经在线
-		route_log "已经在线"
-		tmp_led_status="off"
 		return 0
 	elif [ "$rep" == '{"result":"1","msg":"\u8ba4\u8bc1\u6210\u529f"}' ] ;then
 		# 登录成功
-		if [ "$tmp_led_status" != "off" ] ;then
-			tmp_led_status="off"
-		fi
-		route_log "登录成功"
 		return 1
 	elif [ "$rep" == '{"result":"0","msg":"aW51c2UsIGxvZ2luIGFnYWluLCBwYyBPTG5vIDEgPj0gMQ==","ret_code":4}' ] ;then
 		# 已在其他设备登录
-		route_log "已在其他设备登录"
 		return 2
 	elif [ "$rep" == "" ] ;then
-		get_user_ip
-		if [ $? == 0 ] ;then
-			if [ "$tmp_led_status" != "ip_drops" ] ;then
-				tmp_led_status="ip_drops"
-			fi
-			route_log "error_ct ap失联丢失"
-			return 3
-		fi
-		if [ "$tmp_led_status" != "ap_drops" ] ;then
-				tmp_led_status="ap_drops"
-		fi
-		route_log "error_ap ap网络异常"
-		return 4
+		# ap网络异常
+		return 3
 	fi
-	if [ "$tmp_led_status" != "error" ] ;then
-			tmp_led_status="error"
-	fi
-	return 5
+	# 未知错误error
+	return 4
 }
 
 
 
 # main
+led_lock=0
+led_status="tmp"
 
 while [ "66" == "66" ]
 do
-	route_status=2
-	getnetwork
-	route_status=$?
-	if [ $route_status == 5 ] ;then
-		route_log "error 出现未知异常"
-		sleep 5m
-		continue
+	# 获取网络状态，网络连接失败
+	network_status
+	if [ $? == 0 ] ;then
+		# 仅有led_status为off才会触发
+		if [ $led_lock == 0 ] ;then
+			# set led to connecting
+			route_status_led 'connecting'
+		fi
+		# 获取当前网卡ip，ip不存在return 0
+		get_user_ip
+		if [ $? == 0 ] ;then
+			route_status_led 'ip_drops'
+			sleep 1s
+			continue
+		fi
+		# 登录账号，return code：1 to 5
+		getnetwork
+		tmp_status=$?
+		route_log $tmp_status
+		if [ $tmp_status -le 2 ] ;then
+			continue
+		elif [ $tmp_status == 3 ] ;then
+			route_status_led 'ap_drops'
+			sleep 2s
+			continue
+		elif [ $tmp_status == 4 ] ;then
+			route_status_led 'error'
+			sleep 5m
+			continue
+		fi
 	fi
-	if [ "$tmp_led_status" != "$led_status" ] ;then
-		led_status=$tmp_led_status
-		route_status_led $led_status
-	fi
+	route_status_led 'off'
 	sleep 5s
 done
